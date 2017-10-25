d47896bdd75eabd777ca306df85945d0950f02d6
David Borges <davidborges012@gmail.com>
2017-10-24 23:42:53 -0300

    Drower incompleto

--- Book_Tea/app/src/main/java/br/edu/ifpe/tads/pdm/book_tea/MainActivity.java
+++ Book_Tea/app/src/main/java/br/edu/ifpe/tads/pdm/book_tea/MainActivity.java
@@ -5,23 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import br.edu.ifpe.tads.pdm.book_tea.domain.Livro;
 import java.util.List;
 import java.util.ArrayList;
 
-import br.edu.ifpe.tads.pdm.book_tea.R;
 import br.edu.ifpe.tads.pdm.book_tea.fragments.LivroFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mainToolbar;
-    private Drawer.Result navigationDrawerLeft;
-    private Drawer.Result navigationDrawerRigt;
-    private AccountHeader.Result headerNavigationLeft;
+    private Drawer.Result navigationDrawer;
+    private AccountHeader.Result headerNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,18 +48,37 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.commit();
         }
 
-//        navigationDrawerLeft = new Drawer()
-//                .withActivity(this)
-//                .withToolbar(mainToolbar)
-//                .withDisplayBelowToolbar(false)
-//                .withActionBarDrawerToggleAnimated(true)
-//                .withDrawerGravity(Gravity.LEFT)
-//                .withSavedInstance(savedInstanceState)
-//                .withSelectedItem(0)
-//                .withActionBarDrawerToggle(true)
-//                .withAccountHeader(headerNavigationLeft)
-    }
+        navigationDrawer = new Drawer()
+                .withActivity(this)
+                .withToolbar(mainToolbar)
+                .withDisplayBelowToolbar(false)
+                .withActionBarDrawerToggleAnimated(true)
+                .withDrawerGravity(Gravity.LEFT)
+                .withSavedInstance(savedInstanceState)
+                .withSelectedItem(0)
+                .withActionBarDrawerToggle(true)
+                .withAccountHeader(headerNavigation)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
 
+                    }
+                })
+                .withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+                        Toast.makeText(MainActivity.this, "onItemLongClick: " + i, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                })
+                .build();
+        navigationDrawer.addItem(new PrimaryDrawerItem().withName("Opção A").withIcon(getResources().getDrawable(R.drawable.logo_book_tea_min)));
+        navigationDrawer.addItem(new PrimaryDrawerItem().withName("Opção B").withIcon(getResources().getDrawable(R.drawable.logo_book_tea_min)));
+        navigationDrawer.addItem(new PrimaryDrawerItem().withName("Opção C").withIcon(getResources().getDrawable(R.drawable.logo_book_tea_min)));
+        navigationDrawer.addItem(new PrimaryDrawerItem().withName("Opção D").withIcon(getResources().getDrawable(R.drawable.logo_book_tea_min)));
+        navigationDrawer.addItem(new DividerDrawerItem());
+        navigationDrawer.addItem(new SectionDrawerItem().withName("Configurações"));
+    }
 
     //Metodo de teste para modelo navegacional
     public List<Livro> listBookSimulator(int quantidade){
